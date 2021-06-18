fb570a253c13cd456d7269d7cc47e99e74e9cd43
Muhammad Idrees <m.idrees@codelabs.inc>
2021-06-18 15:21:46 +0500

    git initizlization check lag gaya

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,10 +14,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('no-commit.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('no-commit.initialize', () => {
+
+		// checks if git is initialized
+		if (fs.existsSync(".git")) {
+			// File exists in path
+			console.log("cool");
+		  } else {
+			  vscode.window.showWarningMessage('Git not initialized!');
+		  }
+		  
+
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from No Commit!');
+		vscode.window.showInformationMessage('No commit initialized Successfully!');
 	});
 
 	context.subscriptions.push(disposable);
